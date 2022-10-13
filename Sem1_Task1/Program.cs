2ed52b26578efcacdfa029f1c3a78266f75c0e77
Natalya <andreyevani@yandex.ru>
2022-10-13 20:55:39 +0300

    add E:\Example\Sem1_Task1

---
+++ Sem1_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿System.Console.WriteLine("Введите номер дня недели"); //выводим подсказку на экран
+string inputString = Console.ReadLine(); //получаем строку с консоли
+int dayOfWeek = Convert.ToInt32(inputString ); //конвертируем к целому типу
+
+if (dayOfWeek == 1)
+{
+      Console.WriteLine("Понедельник" );
+}
+if (dayOfWeek == 2)
+{
+      Console.WriteLine("Вторник" );
+}
+if (dayOfWeek == 3)
+{
+      Console.WriteLine("Среда" );
+}
+
+if (dayOfWeek == 4)
+{
+      Console.WriteLine("Четверг" );
+}
+if (dayOfWeek == 5)
+{
+      Console.WriteLine("Пятница" );
+}
+if (dayOfWeek == 6)
+{
+      Console.WriteLine("Суббота" );
+}
+if (dayOfWeek == 7)
+{
+      Console.WriteLine("Воскресенье" );
+}
+else
+{
+    Console.WriteLine("Неправильно. нет такого дня" );
+}

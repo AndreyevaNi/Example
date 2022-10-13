9d7ca2f2607dcbb616d4309ca11a95f7c6af99b7
Natalya <andreyevani@yandex.ru>
2022-10-13 21:11:03 +0300

    add Sem1_Task2

---
+++ Sem1_Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите число"); 
+string inputString = Console.ReadLine(); 
+int value = Convert.ToInt32(inputString ); 
+int Cube = value * value* value; 
+System.Console.WriteLine(Cube); 

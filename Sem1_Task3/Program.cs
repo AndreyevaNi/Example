56c7dfd7536a5b4c04d975c613638bb4799be482
Natalya <andreyevani@yandex.ru>
2022-10-13 21:22:29 +0300

    add Tas3

---
+++ Sem1_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+string inputString = Console.ReadLine();
+int Number = Convert.ToInt32(inputString);
+int count = Number * -1;
+
+while (count < number)
+{
+    System.Console.Write(count + " ");
+    count++;
+
+}
+

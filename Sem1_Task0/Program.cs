3ae9add52308f1f3db67fa7acb9c62afab803e19
Natalya <andreyevani@yandex.ru>
2022-10-13 20:29:32 +0300

    add Sem1_Task0

---
+++ Sem1_Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число"); //выводим подсказку на экран
+string inputString = Console.ReadLine(); //получаем строку с консоли
+int value = Convert.ToInt32(inputString ); //конвертируем к целому типу
+int square = value * value; //вычисляем квадрат числа
+System.Console.WriteLine(square); //вывод на экран
+
+
+
+

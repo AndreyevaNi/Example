3267f2c47a3c70f071cd4367baa8d72c29bcf059
Natalya <andreyevani@yandex.ru>
2022-10-13 21:45:13 +0300

    add Sem1_Task4

---
+++ Sem1_Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿//**. Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+//1 -> 1 
+//2 -> 0.5
+//0.25 -> 4
+
+System.Console.WriteLine("Введите число"); 
+string inputString = Console.ReadLine(); 
+double Number = Convert.ToDouble(inputString ); 
+double  ReversNumber= 1/Number; 
+System.Console.WriteLine($"{ReversNumber:F2}"); //Округление, 2 знака после запятой.

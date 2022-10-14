ad2f33b2877ae81d6315383daf76d7adabdf3155
Natalya <andreyevani@yandex.ru>
2022-10-14 12:04:13 +0300

    add HomeWork_1_Task1_MaxMinNumbers

---
+++ HomeWork_1/Task_1_MaxMinNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//Пример:
+//a = 5; b = 7 ->  max = 7, min = 5
+//a = -9 b = -3 -> max = -3, min = -9
+
+// *********Начало. Блок ввода двух чисел и преобразование их к вещественному типу*****//
+System.Console.WriteLine("Введите первое число");
+string inputString = Console.ReadLine();
+double Number1 = Convert.ToDouble(inputString);
+//inputString = "";
+System.Console.WriteLine("Введите первое число");
+inputString = Console.ReadLine();
+double Number2 = Convert.ToDouble(inputString);
+// *********Конец. Блок ввода двух чисел и преобразование их к вещественному типу*****//
+
+if (Number1 > Number2)
+{
+      System.Console.WriteLine("Число: "+ Number1+ " больше, чем число: " + Number2);
+
+}
+else 
+{
+    System.Console.WriteLine("Число: "+ Number2+ " больше, чем число: " + Number1);
+}
